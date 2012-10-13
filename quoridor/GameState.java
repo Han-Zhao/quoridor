c6f8efd264c71e6d32938161566d83bbf822c045
Han Zhao <hzha533@cse.unsw.EDU.AU>
2012-10-13 18:07:36 +1100

    1

--- quoridor/GameState.java
+++ quoridor/GameState.java
@@ -8,13 +8,15 @@ public class GameState {
 	Player player1;
 	Player player2;
 	Player turn;
-	HashMap<Position, Orientation> allWalls;
+	Player other;
+	HashMap<Position, Orientation> walls;
 	
 	public GameState() {
 		player1 = new Player(new Position("e9"));
 		player2 = new Player(new Position("e1"));
 		turn = player1;
-		allWalls = new HashMap<Position, Orientation>();
+		other = player2;
+		walls = new HashMap<Position, Orientation>();
 	}
 	
 	public boolean parse(String move) {
@@ -23,18 +25,29 @@ public class GameState {
 		if (!(move.charAt(0) >= 'a' && move.charAt(0) <= 'i' && move.charAt(1) >= '1' && move.charAt(1) <= '9'))
 			return false;
 		if (move.length() == 3) {
+			if (move.charAt(0) == 'i' || move.charAt(1) == '9')
+				return false;
 			if (move.charAt(2) == 'v')
-				allWalls.put(new Position(move), Orientation.VERTICAL);
+				walls.put(new Position(move), Orientation.VERTICAL);
 			else if (move.charAt(2) == 'h')
-				allWalls.put(new Position(move), Orientation.HORIZONTAL);
+				walls.put(new Position(move), Orientation.HORIZONTAL);
 			else
 				return false;
 			turn.wallsCount++;
-		} else if (move.length() == 2)
-			turn.move(new Position(move));
-		else
+		} else if (move.length() == 2) {
+			Position movement = new Position(move);
+			if (movement.equals(player1.currentPosition) || movement.equals(player2.currentPosition))
+				return false;
+			if (movement.isAdjacent(turn.currentPosition))
+				turn.move(movement);
+			else if (movement.isAdjacent(other.currentPosition) && other.currentPosition.isAdjacent(turn.currentPosition)) {
+				
+			}
+		} else
 			return false;
-		turn = turn == player1 ? player2: player1;
+		Player tmp = turn;
+		turn = other;
+		other = tmp;
 		return true;
 	}
 	
@@ -48,15 +61,15 @@ public class GameState {
 				System.out.print("   ");
 			for (j = 0; j < 9; j++) {
 				if (i % 2 == 0) {
-					if (allWalls.get(new Position (i/2 - 1, j)) == Orientation.HORIZONTAL 
-							|| allWalls.get(new Position (i/2 - 1, j - 1)) == Orientation.HORIZONTAL) {
+					if (walls.get(new Position (i/2 - 1, j)) == Orientation.HORIZONTAL 
+							|| walls.get(new Position (i/2 - 1, j - 1)) == Orientation.HORIZONTAL) {
 						System.out.print("+###");
 					} else {
 						System.out.print("+---");
 					}
 				} else {
-					if (allWalls.get(new Position (i/2, j - 1)) == Orientation.VERTICAL 
-							|| allWalls.get(new Position (i/2 - 1, j - 1)) == Orientation.VERTICAL) {
+					if (walls.get(new Position (i/2, j - 1)) == Orientation.VERTICAL 
+							|| walls.get(new Position (i/2 - 1, j - 1)) == Orientation.VERTICAL) {
 						System.out.print("#");
 					} else {
 						System.out.print("|");
